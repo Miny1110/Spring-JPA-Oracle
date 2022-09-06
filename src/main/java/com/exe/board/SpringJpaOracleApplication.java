62f83657588bc16e435ffb1c13d4cfdc4306df4f
Miny1110 <wjdalswjd453@naver.com>
2022-09-07 08:42:47 +0900

    0906 수업

---
+++ src/main/java/com/exe/board/SpringJpaOracleApplication.java
@@ -0,0 +1,13 @@
+package com.exe.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaOracleApplication.class, args);
+	}
+
+}
